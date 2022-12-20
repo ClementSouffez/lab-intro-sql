62c7d1a3009676a4372c0d3f17a170667cbfdc83
ClementSouffez <clement.souffez@yahoo.com>
2022-12-20 19:43:04 +0100

    update

--- .github/lab_intro_sql.sql
+++ .github/lab_intro_sql.sql
@@ -1,44 +1,44 @@
 USE sakila;
 
 
--- Get all the data from tables actor,film and customer
-
-
+-- 2-Get all the data from tables actor,film and customer
 
 SELECT * FROM actor;
 SELECT * FROM film;
 SELECT * FROM customer;
 
--- Get film titles
+-- 3-Get film titles
 
 SELECT*
 from film;
 where title;
 
 
--- Get unique list of film languages under the alias language. Note that we are not asking 
+-- 4 -Get unique list of film languages under the alias language. Note that we are not asking 
 -- you to obtain the language per each film, but this is a good time to think 
 -- about how you might get that information in the future.
 
 
-SELECT*
-FROM language;
+SELECT name,count(distinct language_id) AS language,name
+FROM language
+group by name
 
 
 
--- How many stores does the company have?
+-- 5-1-How many stores does the company have?
 
-SELECT count(store);
+SELECT count(DISTINCT store_id) as number_of_stores
+FROM store
 
+-- 5.2-how many employees staff does the company have?
 
--- how many employees staff does the company have?
-
-SELECT* 
-FROM staff;
-
-
--- Return a list of employee first names only
+SELECT count(staff_id) as number_of_employees
+FROM staff
 
 
-SELECT*
-FROM staff;
+-- 5.3-Return a list of employee first names only
+
+
+SELECT first_name
+FROM staff
+GROUP BY first_name
